94b61760fab61b684b44bbe3af1b6ebc2572f4ba
Nick <nickxz149@gmail.com>
2019-10-17 15:29:19 -0400

    .

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from src.decryption import decryption
 
 KKey = key.KKey
 VKey = key.VKey
-test = 'This is the most awesome test!'
 
-print(encryption(test, KKey))
-print(decryption(encryption(test, KKey), VKey))
+# # testing
+# test = 'This is the most awesome test!'
+
+# print(encryption(test, KKey))
+# print(decryption(encryption(test, KKey), VKey))
